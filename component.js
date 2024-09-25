9c00928aa0db4464a1fe1aaae5474d0b852aae31
carlosgaliano <galiano111@gmail.com>
2024-09-25 00:29:26 -0500

    retorno

--- component.js
+++ component.js
@@ -1,3 +1,28 @@
 class SaludosBasicoElement extends HTMLElement {
-    
+   constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this.saludo = "Hola, que tal!";
+      this.pintado = false;
+   }
+
+   attributeChangedCallback(nombre, valueOld, valueNew) {
+      if(nombre === 'nombre'){
+         this.saludo = `Hola, ${valueNew}`;
+      }
+      if(this.pintado){
+         this.innerHTML = this.saludo;
+      }
+      console.log(`${nombre} ha cambiado de ${valueOld} a ${valueNew}`)
+   }
+
+   connectedCallback() {
+      this.pintado = true;
+      this.innerHTML = this.saludo;
+   }
+
+   static get observedAttributes() {
+      return ['nombre']
+   }
 }
+customElements.define("saludo-basico", SaludosBasicoElement);
